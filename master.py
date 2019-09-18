eb9812240efbcf089e47e40b03dc9a3add1c3ec9
MatsAndT <matsandreas@tonnesland.name>
2019-09-18 21:45:02 +0200

    rpyc connect to robot

--- master.py
+++ master.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 import rpyc
+
+#TODO add ip adress
+c_head = rpyc.connect("localhost", 18861)
+robot_head = c_head.root
+
+#TODO add ip adress
+c_left = rpyc.connect("localhost", 18862)
+robot_left = c_left.root
+
+#TODO add ip adress
+c_right = rpyc.connect("localhost", 18863)
+robot_right = c_right.root
