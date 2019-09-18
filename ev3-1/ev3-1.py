d7ae908fcc4c9e7b16634609cf18b74d047c72f9
Mats Andreas Tønnesland <matsandreas@tonnesland.name>
2019-09-18 14:45:33 +0200

    rpyc service, io and candy_throw

--- ev3-1/ev3-1.py
+++ ev3-1/ev3-1.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
-import ev3dev2
 
-class Forword():
-    motor = ev3dev2.motor
+"""
+For ev3 which controls motor for candy and color sensor
+"""
 
-    def forword_on(self, speed):
-        pass
+import rpyc
+import time
+from ev3dev2.motor import MediumMotor, OUTPUT_A, OUTPUT_B
+from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sensor import INPUT_1, INPUT_2
 
-        def forword_leght(self, speed, leght):
-            pass
 
-    def forword_roted(self, speed, roted):
-        pass
+class RobotRight(rpyc.Service):
+    def __init__(self, *args, **kwargs):
+        self.exposed_candy_loader = MediumMotor(OUTPUT_A)
+        self.exposed_candy_thrower = MediumMotor(OUTPUT_B)
+        self.exposed_left_cs = ColorSensor(INPUT_1)
+        self.exposed_right_cs = ColorSensor(INPUT_2)
 
-    def forword_off(self):
-        pass
+        super().__init__(*args, **kwargs)
+    
+    def exposed_candy_throw(self):
+        # TODO fix value
+        ### Loade the candy in the thrower
+        self.exposed_candy_loader.on_for_degrees(80,75)
+        self.exposed_candy_loader.on_for_degrees(80,-75)
 
-def turn(degress):
-    pass
-
-class Gyro():
-    gyro = ev3dev2.sensor.lego.GyroSensor(ev3dev2.sensor.INPUT_1)
-    def calibrait(self):
-        pass
-
-    def get_pos(self):
-        pass
+        ### Throwe the candy
+        self.exposed_candy_thrower.on_for_degrees(100,90)
+        self.exposed_candy_thrower.on_for_degrees(50,-90)
